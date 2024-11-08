86df65151b829d53f972860c8e25ef16fff7297c
Di Qi <di@lantern.dev>
2024-11-07 23:36:32 -0800

    Add L2 and cosine distance

--- lib/lantern/model.rb
+++ lib/lantern/model.rb
@@ -3,15 +3,24 @@ module Lantern
     def has_neighbors(*attribute_names)
       attribute_names.map!(&:to_sym)
 
-      scope :nearest_neighbors, ->(attribute_name, vector) {
+      scope :nearest_neighbors, ->(attribute_name, vector, distance: 'l2') {
         attribute_name = attribute_name.to_sym
 
         unless vector.all? { |v| v.is_a?(Integer) }
             vector = vector.map(&:to_f)
         end
-
         vector_literal = vector.join(',')
-        order_expression = "#{quoted_table_name}.#{connection.quote_column_name(attribute_name)} <-> ARRAY[#{vector_literal}]"
+
+        distance_operators = {
+          'l2' => '<->',
+          'cosine' => '<=>'
+        }
+        operator = distance_operators[distance]
+        unless operator
+          raise ArgumentError, "Invalid distance metric. Allowed metrics are #{distance_operators.keys.join(', ')}"
+        end
+
+        order_expression = "#{quoted_table_name}.#{connection.quote_column_name(attribute_name)} #{operator} ARRAY[#{vector_literal}]"
 
         select_columns = select_values.any? ? [] : column_names
         select(select_columns, "#{order_expression} AS distance")
@@ -19,11 +28,11 @@ module Lantern
           .order(Arel.sql(order_expression))
       }
 
-      define_method :nearest_neighbors do |attribute_name|
+      define_method :nearest_neighbors do |attribute_name, distance: 'l2'|
         attribute_name = attribute_name.to_sym
         self.class
           .where.not(id: id)
-          .nearest_neighbors(attribute_name, self[attribute_name])
+          .nearest_neighbors(attribute_name, self[attribute_name], distance: distance)
       end
     end
   end
--- test/model_test.rb
+++ test/model_test.rb
@@ -18,8 +18,8 @@ class LanternTest < Minitest::Test
 
     @vectors = [
       [1.0, 0.0, 0.0],
-      [0.0, 1.0, 0.0],
-      [0.0, 0.0, 1.0]
+      [0.0, 0.5, 0.0],
+      [1.0, 0.0, 0.2]
     ]
 
     @vectors.each do |vector|
@@ -27,32 +27,92 @@ class LanternTest < Minitest::Test
     end
   end
 
-  def test_instance_nearest_neighbors
-    item = Item.first
-    neighbors = item.nearest_neighbors(:embedding).limit(2)
-  
-    assert_equal 2, neighbors.length
-    assert_equal [2, 3], neighbors.pluck(:id)
+  # Helper method to test nearest neighbors
+  def assert_nearest_neighbors(test_case)
+    item = Item.find(test_case[:item_id])
+    neighbors = item.nearest_neighbors(:embedding, distance: test_case[:distance_metric]).limit(2)
 
-    distances = neighbors.map { |neighbor| neighbor.distance }
-    expected_distances = [2.0, 2.0]
-    assert_equal expected_distances, distances
+    assert_equal test_case[:expected_neighbor_ids], neighbors.pluck(:id)
+    distances = neighbors.map(&:distance)
+
+    test_case[:expected_distances].each_with_index do |expected_distance, index|
+      if expected_distance.is_a?(Float)
+        assert_in_delta expected_distance, distances[index], 1e-6, "Distance mismatch for neighbor #{neighbors[index].id}"
+      else
+        assert_equal expected_distance, distances[index], "Distance mismatch for neighbor #{neighbors[index].id}"
+      end
+    end
   end
-  
-  def test_class_nearest_neighbors
-    neighbors = Item.nearest_neighbors(:embedding, [0.5, 0.0, 0.5]).limit(2)
 
-    assert 2, neighbors.length
-    assert_equal [1, 3], neighbors.pluck(:id)
+  # Test cases for different distance metrics
+  DISTANCE_TEST_CASES = [
+    {
+      description: "L2 Distance (instance method)",
+      item_id: 1,
+      distance_metric: 'l2',
+      expected_neighbor_ids: [3, 2],
+      expected_distances: [0.04, 1.25]
+    },
+    {
+      description: "Cosine Distance (instance method)",
+      item_id: 1,
+      distance_metric: 'cosine',
+      expected_neighbor_ids: [3, 2],
+      expected_distances: [0.0194193243, 1.0]
+    }
+  ]
 
-    distances = neighbors.map { |neighbor| neighbor.distance }
-    expected_distances = [0.5, 0.5]
-    assert_equal expected_distances, distances
+  # Dynamically define test methods for instance methods
+  DISTANCE_TEST_CASES.each_with_index do |test_case, index|
+    define_method("test_instance_nearest_neighbors_#{test_case[:distance_metric]}") do
+      assert_nearest_neighbors(test_case)
+    end
+  end
+
+  # Similar helper method for class methods
+  def assert_class_nearest_neighbors(test_case)
+    neighbors = Item.nearest_neighbors(:embedding, test_case[:query_vector], distance: test_case[:distance_metric]).limit(2)
+
+    assert_equal test_case[:expected_neighbor_ids], neighbors.pluck(:id)
+    distances = neighbors.map(&:distance)
+
+    test_case[:expected_distances].each_with_index do |expected_distance, index|
+      if expected_distance.is_a?(Float)
+        assert_in_delta expected_distance, distances[index], 1e-6, "Distance mismatch for neighbor #{neighbors[index].id}"
+      else
+        assert_equal expected_distance, distances[index], "Distance mismatch for neighbor #{neighbors[index].id}"
+      end
+    end
+  end
+
+  # Test cases for class methods
+  CLASS_DISTANCE_TEST_CASES = [
+    {
+      description: "L2 Distance (class method)",
+      query_vector: [0.5, 0.0, 0.2],
+      distance_metric: 'l2',
+      expected_neighbor_ids: [3, 1],
+      expected_distances: [0.25, 0.29]
+    },
+    {
+      description: "Cosine Distance (class method)",
+      query_vector: [0.5, 0.0, 0.2],
+      distance_metric: 'cosine',
+      expected_neighbor_ids: [3, 1],
+      expected_distances: [0.01671799501, 0.07152330911]
+    }
+  ]
+
+  # Dynamically define test methods for class methods
+  CLASS_DISTANCE_TEST_CASES.each_with_index do |test_case, index|
+    define_method("test_class_nearest_neighbors_#{test_case[:distance_metric]}") do
+      assert_class_nearest_neighbors(test_case)
+    end
   end
 
   def test_invalid_dimensions
     assert_raises(ActiveRecord::StatementInvalid) do
-      Item.nearest_neighbors(:embedding, [1.0, 0.0]).limit(2).to_a
+      Item.nearest_neighbors(:embedding, [1.0, 0.0], distance: 'l2').limit(2).to_a
     end
   end
 
